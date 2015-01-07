01906edb47cdc9521c0f660e36bd2bf2af168ea0
fredetchells <fredetchells@yahoo.com>
2015-01-07 17:06:38 +0000

    Create ServerGUI.java

---
+++ ServerGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/*
+ * The server as a GUI
+ */
+public class ServerGUI extends JFrame implements ActionListener, WindowListener {
+	
+}
